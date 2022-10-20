be27c5eba159760d8af6f83434a0b7273d54f065
adminsmpn1grob <suwardi69@admin.smp.belajar.id>
2022-10-20 12:43:19 +0700

    Mengubah title

--- application/views/psp/coba.php
+++ application/views/psp/coba.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Website PSP SMP N 1 Grobogan</title>
+        <title>Website PSP SMP N 1 GROBOGAN</title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Bootstrap icons-->
